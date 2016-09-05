e002a3f394596ad0450088b938862e171ec9657b
stevemasteller <stevermasteller@gmail.com>
2016-09-05 07:25:03 -0400

    Added dummy quotes data array
    Added dummy quotes data array in quotes.js. Added src to quotes.js in
    index.html.

---
+++ js/quotes.js
@@ -0,0 +1,28 @@
+quotes = [
+	{
+		quote: "Example quote #1",
+		source: "Steve Masteller #1",
+		citation: "Example Citation #1",
+		year: 2016,
+	},
+	{
+		quote: "Example quote #2",
+		source: "Steve Masteller #1",
+		citation: "Example Citation #1",
+		year: 2015,
+	},
+	{
+		quote: "Example quote #3",
+		source: "Steve Masteller #1",
+		year: 2014,
+	},
+	{
+		quote: "Example quote #4",
+		source: "Steve Masteller #1",
+		citation: "Example Citation #1",
+	},
+	{
+		quote: "Example quote #5",
+		source: "Steve Masteller #1",
+	},
+]
