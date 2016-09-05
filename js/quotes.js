7ed798fec9023845b2c458d651ef83eb2d230230
stevemasteller <stevermasteller@gmail.com>
2016-09-05 18:54:48 -0400

    Added real quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,5 +1,67 @@
 var originalQuotes = [
 	{
+		quote: "Politics determine who has the power, not who has the truth.",
+		source: "Paul Krugman",
+		citation: "The Australian Financial Review",
+		year: 2010,
+		tag: "Politics",
+	},
+	{
+		quote: "I used to say that politics was the second-oldest profession. I have come to know that it bears a gross similarity to the first.",
+		source: "Ronald Reagan",
+		year: 1977,
+		tag: "Politics",
+	},
+	{
+		quote: "There never was a good war, or a bad peace.",
+		source: "Benjamin Franklin",
+		citation: "Poor Richards Almanac",
+		tag: "War",
+	},
+	{
+		quote: "When the rich wage war, it is the poor who die.",
+		source: "Jean-Paul sartre",
+		citation: "The Devil and the Good Lord",
+		tag: "War",
+	},
+	{
+		quote: "The chief business of the American people is business.",
+		source: "Calvin Coolidge",
+		year: 1925,
+		tag: "Business",
+	},
+	{
+		quote: "No nation was ever ruined by trade.",
+		source: "Benjamin Franklin",
+		tag: "Business",
+	},
+	{
+		quote: "When you come to a fork in the road, take it.",
+		source: "Yogi Berra",
+		tag: "Humor",
+	},
+	{
+		quote: "When virtual reality gets cheaper than dating, society is doomed.",
+		source: "Scott Adams, Dogbert",
+		tag: "Humor",
+	},
+	{
+		quote: "A family in harmony will prosper in everything.",
+		source: "Chinese Proverb",
+		tag: "Family",
+	},
+	{
+		quote: "All happy families resemble each other, each unhappy family is unhappy in its own way.",
+		source: "Leo Tolstoy",
+		citation: "Anna Karenina",
+		tag: "Family",
+	},
+]
+
+
+/* Test array
+var originalQuotes = [
+	{
 		quote: "Example quote #0",
 		source: "Steve Masteller #0",
 		citation: "Example Citation #0",
@@ -28,6 +90,7 @@ var originalQuotes = [
 	{
 		quote: "Example quote #4",
 		source: "Steve Masteller #4",
-		tag: "Politics",
+		tag: "Gaming",
 	},
 ]
+*/
