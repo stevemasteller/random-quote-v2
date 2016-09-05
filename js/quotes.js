1c4a1d1b58c668f9e292cb122e11172ed1420b6e
stevemasteller <stevermasteller@gmail.com>
2016-09-05 07:48:46 -0400

    Added function getRandomQuotes
    Also added function getRandomNumber to support it.

--- js/quotes.js
+++ js/quotes.js
@@ -1,28 +1,28 @@
 quotes = [
 	{
-		quote: "Example quote #1",
-		source: "Steve Masteller #1",
-		citation: "Example Citation #1",
+		quote: "Example quote #0",
+		source: "Steve Masteller #0",
+		citation: "Example Citation #0",
 		year: 2016,
 	},
 	{
-		quote: "Example quote #2",
+		quote: "Example quote #1",
 		source: "Steve Masteller #1",
 		citation: "Example Citation #1",
 		year: 2015,
 	},
 	{
-		quote: "Example quote #3",
-		source: "Steve Masteller #1",
+		quote: "Example quote #2",
+		source: "Steve Masteller #2",
 		year: 2014,
 	},
 	{
-		quote: "Example quote #4",
-		source: "Steve Masteller #1",
-		citation: "Example Citation #1",
+		quote: "Example quote #3",
+		source: "Steve Masteller #3",
+		citation: "Example Citation #3",
 	},
 	{
-		quote: "Example quote #5",
-		source: "Steve Masteller #1",
+		quote: "Example quote #4",
+		source: "Steve Masteller #4",
 	},
 ]
--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
+
+
+// placeholder 
+function printQuote() {
+}
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// Get a random number from 0 to upper - 1
+function getRandomNumber( upper ) {
+  var randomNumber = Math.floor( Math.random() * upper ); 
+  return randomNumber;
+}
+
+// Get a random quote object from quotes array
+function getRandomQuote( quotes ) {
+	var randomIndex = getRandomNumber( quotes.length );
+	return quotes[randomIndex];
+}
+
+var quote = getRandomQuote(quotes);
+console.log(quote.quote);
