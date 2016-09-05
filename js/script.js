0240a02446468d56bd7bd227aabe2bc05e26ada3
stevemasteller <stevermasteller@gmail.com>
2016-09-05 08:43:20 -0400

    Completed getRandomQuote function
    getRandomQuote function gets a random quote and prints it to the screen.
    Added anonymous function to call in order to pass argument.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-
-
-// placeholder 
-function printQuote() {
+// Print some HTML to the screen at id
+function print( id, html ) {
+	document.getElementById(id).innerHTML = html;
 }
-
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 // Get a random number from 0 to upper - 1
 function getRandomNumber( upper ) {
-  var randomNumber = Math.floor( Math.random() * upper ); 
-  return randomNumber;
+	var randomNumber = Math.floor( Math.random() * upper ); 
+	return randomNumber;
 }
 
 // Get a random quote object from quotes array
@@ -20,5 +15,29 @@ function getRandomQuote( quotes ) {
 	return quotes[randomIndex];
 }
 
-var quote = getRandomQuote(quotes);
-console.log(quote.quote);
+// Call getRandomQuote
+// Construct HTML string from quote object
+// 		Skip citation if it doesn't exist
+//		Skip year if it doesn't exist
+// Call print 
+function printQuote( quotes ) {
+	var quote = getRandomQuote( quotes );
+	var html  = ' <p class="quote">' + quote.quote +
+				'</p> <p class="source">' + quote.source;
+	
+	if ('citation' in quote) {
+		html += '<span class="citation">' + quote.citation + '</span>';
+	}
+	
+	if ('year' in quote) {
+		html += '<span class="year">' + quote.year + '</span>';
+	}
+	
+	html += '</p>';
+	print( 'quote-box', html );
+}
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", function() {printQuote( quotes )}, false);
+
